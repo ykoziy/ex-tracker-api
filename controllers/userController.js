cbdf800a586c73314c02a8b334f8a725f3df75e1
Yuriy Koziy <yukoziy@gmail.com>
2019-10-05 17:41:41 -0400

    Added userController

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/user');
+const  moment = require("moment");
+
+exports.createUser = (req, res) => {
+  res.send('POST: createUser not implemented');
+}
+
+exports.addExcercise = (req, res) => {
+  res.send('POST: addExcercise not implemented');
+}
+
+exports.getExcerciseLog = (req, res) => {
+  res.send('GET: getExcerciseLog not implemented');
+}

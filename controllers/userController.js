84302c4203c33f208221d7ff6b8b3197cd5ec9ed
Yuriy Koziy <yukoziy@gmail.com>
2019-10-05 20:47:44 -0400

    Users can now be added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
 const User = require('../models/user');
 const  moment = require("moment");
 
-exports.createUser = (req, res) => {
-  res.send('POST: createUser not implemented');
+exports.createUser = (req, res, next) => {
+  const user = new User(req.body);
+  user.save((err, data) => {
+    if (err) {
+      if(err.code === 11000) {
+        return next({status: 400, message: 'username taken'})
+      } else {
+        return next(err);
+      }
+    } else {
+      res.json({username: data.username, uid: data.uid});
+    }
+  });
 }
 
 exports.addExcercise = (req, res) => {

fe5f2d71c518cd73ef8b422a1f335f5cd982f7b5
Yuriy Koziy <yukoziy@gmail.com>
2019-10-07 23:00:15 -0400

    Small fix to use the renamed file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userController = require('../../controllers/userController');
+const userController = require('../../controllers/user');
 
 router.post('/exercise/new-user', userController.createUser);
 router.post('/exercise/add', userController.addExercise);

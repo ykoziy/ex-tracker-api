462f00b1b48e44fc846cca20f2c07cf86df924ea
Yuriy Koziy <yukoziy@gmail.com>
2019-12-20 15:29:46 -0500

    Fixed User model, log field now references Exercise

--- models/User.js
+++ models/User.js
@@ -1,23 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const shortid = require("shortid");
-const moment = require("moment");
-
-const getCurrentDate = () => {
-  return moment().format('YYYY-MM-DD');
-}
 
 let UserSchema = new Schema({
   _id: {type: String, default: shortid.generate},
   newdate: {type: Date, default: Date.now},
   username: {type: String, unique: true, required: true},
-  exercise: [
-    {
-      description: String,
-      duration: String,
-      date: {type: Date, default: getCurrentDate}
-    }
-  ]
+  log: [{type: Schema.Types.ObjectId, ref: 'Story'}]
 });
 
 module.exports = mongoose.model('User', UserSchema);

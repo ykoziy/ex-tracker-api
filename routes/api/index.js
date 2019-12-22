b9e320265f5256a7bb80184cfbb45fbde59c5d32
Yuriy Koziy <yukoziy@gmail.com>
2019-12-22 18:45:44 -0500

    Added route /api/exercise/entry?{exId} that gets exercise entry.

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ const userController = require('../../controllers/user');
 router.post('/exercise/new-user', userController.createUser);
 router.post('/exercise/add', userController.addExercise);
 router.get('/exercise/log', userController.getExerciseLog);
+router.get('/exercise/entry', userController.getExerciseEntry);
 router.get('/exercise/users', userController.getUsers);
 
 module.exports = router;

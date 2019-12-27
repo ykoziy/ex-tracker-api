09147c6ec88ff76b5f79be8901a6dc9eacf89460
Yuriy Koziy <yukoziy@gmail.com>
2019-12-27 13:28:32 -0500

    Routes now uses main routing.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const api = require('./api');
+const main = require('./main');
 
+routes.use('/', main);
 routes.use('/api', api);
 
 module.exports = routes;

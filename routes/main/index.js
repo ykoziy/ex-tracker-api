a118cef1e6655725366eb0df8470f872e57107f3
Yuriy Koziy <yukoziy@gmail.com>
2019-12-27 14:00:29 -0500

    Added routes for '/u/:userId', for listing users exercises.

--- routes/main/index.js
+++ routes/main/index.js
@@ -4,4 +4,9 @@ router.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+router.get('/u/:userId', (req, res) => {
+  res.sendFile(process.cwd() + '/views/list.html');
+});
+
+
 module.exports = router;

12a29d2e08e12b9f204fe0e05a77cdd28f494945
Yuriy Koziy <yukoziy@gmail.com>
2020-01-03 18:51:27 -0500

    Moved getExerciseEntry into exercise controller. Fixed usage in routes.

--- controllers/exercise.js
+++ controllers/exercise.js
@@ -1,6 +1,16 @@
 const Exercise = require('../models/Exercise');
 const moment = require("moment");
 
+
+exports.getExerciseEntry = (req, res, next) => {
+  const { exId } = req.query;
+  Exercise.findById(exId, '-__v', (err, entry) => {
+    if (err) return next(err);
+    if (!entry) return next({status: 400, message: 'exercise id not found'});
+    res.json(entry);
+  });
+}
+
 exports.editExerciseEntry = (req, res, next) => {
   const { exId } = req.body;
   console.log("Editing exercise: " + exId);
--- routes/api/index.js
+++ routes/api/index.js
@@ -5,7 +5,7 @@ const exerciseController = require('../../controllers/exercise');
 router.post('/exercise/new-user', userController.createUser);
 router.post('/exercise/add', userController.addExercise);
 router.get('/exercise/log', userController.getExerciseLog);
-router.get('/exercise/entry', userController.getExerciseEntry);
+router.get('/exercise/entry', exerciseController.getExerciseEntry);
 router.get('/exercise/users', userController.getUsers);
 router.put('/exercise/edit', exerciseController.editExerciseEntry)
 

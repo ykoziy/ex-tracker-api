f65e57e881999d9c7653611fefea731ef31d9487
Yuriy Koziy <yukoziy@gmail.com>
2020-01-03 18:49:33 -0500

    Added route for handling PUT request, editing a specific exercise.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userController = require('../../controllers/user');
+const exerciseController = require('../../controllers/exercise');
 
 router.post('/exercise/new-user', userController.createUser);
 router.post('/exercise/add', userController.addExercise);
 router.get('/exercise/log', userController.getExerciseLog);
 router.get('/exercise/entry', userController.getExerciseEntry);
 router.get('/exercise/users', userController.getUsers);
+router.put('/exercise/edit', exerciseController.editExerciseEntry)
 
 module.exports = router;
